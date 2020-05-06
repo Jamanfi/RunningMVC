af2144bf6f79974202049cb62b158ae0b0b76479
James Fitzgerald <james.fitzgerald2011@hotmail.co.uk>
2020-05-06 15:42:20 +0100

    Add duration picker for new competitor form

--- wwwroot/js/races.js
+++ wwwroot/js/races.js
@@ -1,25 +1,54 @@
 ﻿$(document).ready(function () {
 
+    var newCompetitor = {
+        runnerId: 0,
+        finishTime: ""
+    };
+
+    $('.duration-picker').durationPicker({
+        showSeconds: true,
+        showDays: false,
+
+        // callback function that triggers every time duration is changed 
+        //   value - duration in seconds
+        //   isInitializing - bool value
+        onChanged: function (value, isInitializing) {
+            // isInitializing will be `true` when the plugin is initialized for the
+            // first time or when it's re-initialized by calling `setValue` method
+            console.log(value, isInitializing);
+            newCompetitor.finishTime = value;
+            console.log(`NEW COMPETITOR FINISH TIME:${formatTimeFromSeconds(value)}`);
+        }
+    });
+
+    function formatTimeFromSeconds(value) {
+        var hours = Math.floor(value / 3600);
+        value = value - hours * 3600;
+        var minutes = Math.floor(value / 60);
+        
+
+        var seconds = value - minutes * 60;
+
+        if (hours < 10) {hours = `0${hours}`;}
+        if (minutes < 10) {minutes = `0${minutes}`;}
+        if (seconds < 10) {seconds = `0${seconds}`;}
+
+        return `${hours}:${minutes}:${seconds}`;
+    }
+
+
     $("#btnNewComp").click(function () {
         $("#newComp").show();
         const url = `http://${window.location.host}/runners/get`;
-        console.log(url);
         var dropdown = $("#selectRunner");
-        console.log("THE DROPDOWN");
-        console.log(dropdown);
-        console.log("SENDING REQUEST");
-
         $.getJSON(url,
             function (data) {
-                console.log("REQUEST SUCCESSFUL");
-                console.log(data);
                 let option;
                 for (let i = 0; i < data.length; i++) {
                     option = document.createElement('option');
                     option.text = data[i].forename + " " + data[i].surname;
                     option.value = data[i].id;
                     dropdown.append(option);
-                    console.log(`DROPDOWN OPTION ADDED: ${data[i].forename} ${data[i].surname}`);
                 }
             });
 
